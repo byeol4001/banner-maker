414c66095c2e40c392b13e681e63f147f5b5e0de
byeol <byeol4001@gmail.com>
2022-02-25 22:58:31 +0900

    feat: 레이아웃 변경, 하단 색 선택 추가

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,14 +1,18 @@
 
-import { useState } from "react";
-import { TwitterPicker } from 'react-color';
+import html2canvas from "html2canvas";
+import { useRef, useState } from "react";
+import { HexColorPicker } from "react-colorful";
+import * as clipboard from 'clipboard-polyfill';
+import { ClipboardItem } from 'clipboard-polyfill';
 
-import {MainWrap,Title,InputBox,Input,BannerBox,BannerTitle,BannerSub,Line} from "./style/Styles"
+import { MainWrap, Title, InputBox, Input, BannerBox, BannerTitle, BannerSub, Line, CopyButton, Devider, PrevWrap} from "./style/Styles"
 
 
 export default function Main() {
   const [title,setTitle] = useState("")
   const [sub,setSub] = useState("")
-  const [lineColor, setLineColor] = useState("#fff")
+  const [lineColor, setLineColor] = useState("#2C705E")
+  const copyBanner:any = useRef()
   const handleTitle = (e: React.FormEvent<HTMLInputElement>)=>{
     setTitle(e.currentTarget.value)
   }
@@ -17,27 +21,46 @@ export default function Main() {
   }
 
   const handleChangeComplete = (color:any) => {
-    setLineColor(color.hex);
-    console.log(typeof color.hex)
+    setLineColor(color);
   };
+
+  const copyToClipboard = () =>{
+    html2canvas(copyBanner.current).then((canvas: HTMLCanvasElement) => {
+      canvas.toBlob((blob:any) => {
+        clipboard.write([new ClipboardItem({ 'image/png': blob })]);
+      })
+  });
+  }
+
+
   return <MainWrap>
     <Title>Banner Maker</Title>
-    <BannerBox>
+    <InputBox>
+    <div>
+      <Input>
+        <label htmlFor="title">Title</label>
+        <input onChange={handleTitle} id="title" placeholder="제목 입력" type="text"/>
+      </Input>
+      <Input>
+        <label htmlFor="sub-title">Desciption</label>
+        <input onChange={handleSub} id="sub-title" placeholder="설명 입력" type="text"/>
+      </Input>
+      </div>
+      <Input>
+        <label htmlFor="sub-title">Select Bottom Color</label>
+        <HexColorPicker color={lineColor} onChange={handleChangeComplete} />
+      </Input>
+    </InputBox>
+    <PrevWrap>
+      <h5>Preview</h5>
+      <CopyButton onClick={copyToClipboard}>copy</CopyButton>
+    </PrevWrap>
+    <Devider/>
+    <BannerBox ref={copyBanner}>
       <BannerTitle>{title}</BannerTitle>
       <BannerSub>{sub}</BannerSub>
-      <Line />
+      <Line color={lineColor}/>
     </BannerBox>
-    <InputBox>
-      <Input>
-        <label htmlFor="title">제목</label>
-        <input onChange={handleTitle} id="title" type="text"/>
-      </Input>
-      <Input>
-        <label htmlFor="sub-title">설명</label>
-        <input onChange={handleSub} id="sub-title" type="text"/>
-      </Input>
-    <TwitterPicker color={lineColor}
-        onChangeComplete={ handleChangeComplete }/>
-    </InputBox>
+    <Devider/>    
   </MainWrap>;
 }
--- src/style/Styles.tsx
+++ src/style/Styles.tsx
@@ -7,21 +7,30 @@ const MainWrap = styled.div`
     flex-direction: column;
     align-items:center;
     justify-content:center;
+    background: ${props => props.theme.bgColor};
 `
 
 const Title = styled.div`
     font-size:60px;
     font-weight: 900;
     margin-bottom:40px;
+    color: ${props => props.theme.fontColor};
+    font-family: 'Montserrat', sans-serif;
 `
 
 const InputBox = styled.div`
     display: flex;
-    gap: 20px;
-    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
     width:100%;
-    max-width:700px;
-    padding-top:50px;
+    max-width:800px;
+    padding-bottom: 40px;
+    & > div:first-child{
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+        width: 530px;
+    }
 `
 
 const Input = styled.div`
@@ -29,7 +38,10 @@ const Input = styled.div`
     flex-direction: column;
     label{
         font-size: 17px;
-        margin-bottom: 5px;
+        margin-bottom: 10px;
+        font-family: 'Montserrat', sans-serif;
+        font-weight:200;
+        color: ${props => props.theme.fontColor};
     }
     input{
         width:100%;
@@ -37,11 +49,22 @@ const Input = styled.div`
         padding: 20px;
         font-size: 20px;
         border-radius: 10px;
+        border:none;
+        background: ${props => props.theme.fontColor};
+        color: #fff;
+        box-shadow: 8px 9px 16px 0px #8CAAA2;
+        &:focus{
+            border:none;
+        }
+        &::placeholder{
+            color:#fff;
+            opacity:0.4;
+        }
     }
 `
 
 const BannerBox = styled.div`
-    width:860px;
+    width:100%;
     max-width:860px;
     height:422px;
     position:relative;
@@ -75,7 +98,46 @@ const Line = styled.div`
     position:absolute;
     bottom:0;
     left:0;
-    background:#B9EB46;
+    background:${props => props.color};
 `
 
-export {MainWrap,Title,InputBox,Input,BannerBox,BannerTitle,BannerSub,Line}
+const CopyButton = styled.div`
+    padding: 9px 13px;
+    border:3px solid;
+    border-color :${props => props.theme.fontColor};
+    color: ${props => props.theme.fontColor};
+    font-weight:900;
+    font-family: inherit;
+    border-radius: 10px;
+    cursor: pointer;
+    transition: all 0.5s ease-in-out ;
+    &:hover {
+        transform: translate(3px, -3px);
+    }
+
+`
+
+const Devider = styled.div`
+    max-width:860px;
+    width:100%;
+    height:2px;
+    margin:15px 0;
+    background :${props => props.theme.fontColor}
+`
+
+const PrevWrap = styled.div`
+    max-width: 840px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-family: 'Montserrat', sans-serif;
+    color: ${props => props.theme.fontColor};
+    & > h5 {
+        font-family: inherit;
+        font-weight:700;
+        font-size:25px;
+    }
+`
+
+export { MainWrap, Title, InputBox, Input, BannerBox, BannerTitle, BannerSub, Line, CopyButton, Devider, PrevWrap} 
